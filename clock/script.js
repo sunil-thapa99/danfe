bf56757220e0df3fa623e685800771bdf6bb8df1
lappanchappan43 <lappanchappan43@gmail.com>
2018-04-25 15:25:36 +0545

    Completed task

--- clock/script.js
+++ clock/script.js
@@ -1,59 +1,32 @@
 const HOURHAND = document.querySelectorAll("#hour");
 const MINUTEHAND = document.querySelectorAll("#minute");
 const SECONDHAND = document.querySelectorAll("#second");
-console.log(HOURHAND, MINUTEHAND);
 const TIME = document.querySelectorAll('#time');
-function getDate(){
+function getDate(x, y){
 	var time = new Date();
 	var utc = time.getTime() + time.getTimezoneOffset() * 60000;
-	var berlin = new Date(utc + (3600000*(+2)));
-	var minutes = berlin.getMinutes();
-	var seconds = berlin.getSeconds();
-	var hr = berlin.getHours();
-	MINUTEHAND[0].style.transform = 'rotate(' + 6*minutes + 'deg)';
-	SECONDHAND[0].style.transform = 'rotate(' + 6*seconds + 'deg)';
-	HOURHAND[0].style.transform = 'rotate(' + 30*hr + 'deg)';
-	TIME[0].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds;
+	var stateTime = new Date(utc + (3600000*(x)))
+	var minutes = stateTime.getMinutes();
+	var seconds = stateTime.getSeconds();
+	var hr = stateTime.getHours();
+	MINUTEHAND[y].style.transform = 'rotate(' + 6*minutes + 'deg)';
+	SECONDHAND[y].style.transform = 'rotate(' + 6*seconds + 'deg)';
+	HOURHAND[y].style.transform = 'rotate(' + 30*hr + 'deg)';
+	TIME[y].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds;
 }
-setInterval(getDate, 1000);
+setInterval(function(){
+		getDate(+10, 0);
+	}, 1000);
+setInterval(function(){
+		getDate(+2, 1);
+	}, 1000);
+setInterval(function(){
+		getDate(-4, 2);
+	}, 1000);
+setInterval(function(){
+		getDate(+5.75, 3);
+	}, 1000);
 
-function getDate1(){
-	var time = new Date();
-	var utc = time.getTime() + time.getTimezoneOffset() * 60000;
-	var sydney = new Date(utc + (3600000*(+10)));
-	var minutes = sydney.getMinutes();
-	var seconds = sydney.getSeconds();
-	var hr = sydney.getHours();
-	MINUTEHAND[1].style.transform = 'rotate(' + 6*minutes + 'deg)';
-	SECONDHAND[1].style.transform = 'rotate(' + 6*seconds + 'deg)';
-	HOURHAND[1].style.transform = 'rotate(' + 30*hr + 'deg)';
-	TIME[1].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds;
-}
-setInterval(getDate1, 1000);
-
-
-function getDate2(){
-	var time = new Date();
-	var utc = time.getTime() + time.getTimezoneOffset() * 60000;
-	var newYork = new Date(utc + (3600000*(-4)));
-	var minutes = newYork.getMinutes();
-	var seconds = newYork.getSeconds();
-	var hr = newYork.getHours();
-	MINUTEHAND[2].style.transform = 'rotate(' + 6*minutes + 'deg)';
-	SECONDHAND[2].style.transform = 'rotate(' + 6*seconds + 'deg)';
-	HOURHAND[2].style.transform = 'rotate(' + 30*hr + 'deg)';
-	TIME[2].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds;
-}
-setInterval(getDate2, 1000);
-
-function getDate3(){
-	var time = new Date();
-	var minutes = time.getMinutes();
-	var seconds = time.getSeconds();
-	var hr = time.getHours();
-	MINUTEHAND[3].style.transform = 'rotate(' + 6*minutes + 'deg)';
-	SECONDHAND[3].style.transform = 'rotate(' + 6*seconds + 'deg)';
-	HOURHAND[3].style.transform = 'rotate(' + 30*hr + 'deg)';
-	TIME[3].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds;
-}
-setInterval(getDate3, 1000);
+// var berlin = new Date(utc + (3600000*(+2)));
+// var sydney = new Date(utc + (3600000*(+10)));
+// var newYork = new Date(utc + (3600000*(-4)));
